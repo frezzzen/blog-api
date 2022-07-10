dde8c784a2ddf21d01d6adb038c3d2629ac1ad90
frezzzen <frezzzen>
2022-07-10 18:12:06 +0400

    Fixing swagger

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ async function bootstrap() {
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Blog API')
     .setVersion('1.0')
-    .addServer(process.env.MODE === 'production' ? 'api' : '')
+    .addServer(process.env.MODE === 'production' ? '/api' : '')
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
